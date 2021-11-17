4ee5b114b891ec41d2123f0dcac8a2a6891c73c9
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-17 23:31:20 +0300

    UI: Map component was created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Map from './components/Map/Map';
 import Layout from './layout/Layout';
 
 function App() {
   return (
     <Layout>
       <Header />
+      <Map />
     </Layout>
   );
 }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./map.scss";
+
+const Map: React.FC = () => {
+  return <div className="map-container">Map</div>;
+};
+
+export default Map;
