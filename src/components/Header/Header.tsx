98045df70f54887bab00c79dc5db372653dedda9
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-18 00:04:24 +0300

    UI: Header component was designed.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./header.scss";
+import ArrowLogo from "../../assets/icon-arrow.svg"
 
 const Header: React.FC = () => {
-  return <div className="header-container">Header</div>;
+  return (
+    <div className="header-container">
+      <h1 className="header-title">IP Address Tracker</h1>
+      <div className="header-input-wrapper">
+        <input placeholder="Search for any IP address or domain" />
+        <button>
+          <img src={ArrowLogo} alt="Button's Arrow Logo"/>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
