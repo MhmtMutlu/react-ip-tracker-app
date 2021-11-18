a5e31425c3e6015b28d77a2040bd3f99e89ad745
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-18 21:46:27 +0300

    UI: Header and HeaderInfo components were edited for responsive design.

--- src/components/Header/HeaderInfo/HeaderInfo.tsx
+++ src/components/Header/HeaderInfo/HeaderInfo.tsx
@@ -7,22 +7,22 @@ const HeaderInfo: React.FC = () => {
       <div className='info-wrapper-container'>
         <div className="sections">
           <h5>IP ADDRESS</h5>
-          <h2>192.168.2.1</h2>
+          <h2>192.212.174.101</h2>
         </div>
         <hr />
         <div className='sections'>
           <h5>LOCATION</h5>
-          <h2>192.168.2.1</h2>
+          <h2>Brooklyn, NY 10001</h2>
         </div>
         <hr />
         <div className='sections'>
           <h5>TIMEZONE</h5>
-          <h2>192.168.2.1</h2>
+          <h2>UTC - 05.00</h2>
         </div>
         <hr />
         <div className='sections'>
           <h5>ISP</h5>
-          <h2>192.168.2.1</h2>
+          <h2>SpaceX Starlink</h2>
         </div>
       </div>
     </div>
