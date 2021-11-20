ccd63aafa03f9d6d406d97d15bfd56203f718c74
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-21 01:23:32 +0300

    Ref: Console log was removed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React, { useContext } from 'react';
-import Header from './components/Header/Header';
-import Loading from './components/Loading/Loading';
-import Map from './components/Map/Map';
-import { IPContext } from './contexts/IPContext';
-import Layout from './layout/Layout';
+import React, { useContext } from "react";
+import Header from "./components/Header/Header";
+import Loading from "./components/Loading/Loading";
+import Map from "./components/Map/Map";
+import { IPContext } from "./contexts/IPContext";
+import Layout from "./layout/Layout";
 
 function App() {
   const { loading } = useContext(IPContext);
-  console.log(loading)
 
   return (
     <Layout>
-      {
-        loading && <Loading />
-      }
+      {loading && <Loading />}
       <Header />
       <Map />
     </Layout>
