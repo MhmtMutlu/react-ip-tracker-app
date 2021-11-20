5b6b4b8a490f355163cc044b67460a526d8fe363
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-11-21 01:05:49 +0300

    UI-Feat: Loading component was created and used in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from './components/Header/Header';
+import Loading from './components/Loading/Loading';
 import Map from './components/Map/Map';
-import IPContextProvider from './contexts/IPContext';
+import { IPContext } from './contexts/IPContext';
 import Layout from './layout/Layout';
 
 function App() {
+  const { loading } = useContext(IPContext);
+  console.log(loading)
+
   return (
-    <IPContextProvider>
-      <Layout>
-        <Header />
-        <Map />
-      </Layout>
-    </IPContextProvider>
+    <Layout>
+      {
+        loading && <Loading />
+      }
+      <Header />
+      <Map />
+    </Layout>
   );
 }
 
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="loading-animation">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
